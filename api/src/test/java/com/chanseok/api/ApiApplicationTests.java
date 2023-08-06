70f8ffda334f5c34016b49c94e525f16e9069dcc
chanseok2323@gmail.com <parkchanseok@bagchanseog-ui-MacBookPro.local>
2023-08-07 01:14:59 +0900

    multi module init

---
+++ api/src/test/java/com/chanseok/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanseok.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
